40362296c05a46e6c05c1d795d20af870819b376
Sebastian Junges <sebastian.junges@ru.nl>
2022-07-26 19:02:28 +0200

    first attempt towards integration

---
+++ tests/pomdp/test_pomdp_cutoffs.py
@@ -0,0 +1,35 @@
+import stormpy
+import stormpy.logic
+import stormpy.pomdp
+from helpers.helper import get_example_path
+
+class TestModelChecking:
+    def test_underapprox_mc_maze_pmax(self):
+        program = stormpy.parse_prism_program(get_example_path("pomdp", "maze_2.prism"))
+        formulas = stormpy.parse_properties_for_prism_program("Pmax=? [ !\"bad\" U \"goal\" ]", program)
+        model = stormpy.build_model(program, formulas)
+        model = stormpy.pomdp.make_canonic(model)
+        options = stormpy.pomdp.BeliefExplorationModelCheckerOptionsDouble(False, True)
+        options.use_explicit_cutoff = True
+        options.size_threshold_init = 10
+        belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options);
+        result = belmc.check(formulas[0].raw_formula, [])
+        print(result.lower_bound)
+        print(result.upper_bound)
+        print(result.induced_mc_from_scheduler)
+        print(result.cutoff_schedulers[0])
+
+    def test_underapprox_mc_maze_rmin(self):
+        program = stormpy.parse_prism_program(get_example_path("pomdp", "maze_2.prism"))
+        formulas = stormpy.parse_properties_for_prism_program("Rmin=? [ F \"goal\" ]", program)
+        model = stormpy.build_model(program, formulas)
+        model = stormpy.pomdp.make_canonic(model)
+        options = stormpy.pomdp.BeliefExplorationModelCheckerOptionsDouble(False, True)
+        options.use_explicit_cutoff = True
+        options.size_threshold_init = 10
+        belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options);
+        result = belmc.check(formulas[0].raw_formula, [])
+        print(result.lower_bound)
+        print(result.upper_bound)
+        print(result.induced_mc_from_scheduler)
+        print(result.cutoff_schedulers[0])
