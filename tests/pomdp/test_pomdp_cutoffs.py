7622faf3d5b987831e683820f9de18fe7c19f958
Sebastian Junges <sebastian.junges@ru.nl>
2022-08-15 18:54:43 +0200

    added maze-concise and additional test to trigger bug

--- tests/pomdp/test_pomdp_cutoffs.py
+++ tests/pomdp/test_pomdp_cutoffs.py
@@ -33,3 +33,18 @@ class TestModelChecking:
         print(result.upper_bound)
         print(result.induced_mc_from_scheduler)
         print(result.cutoff_schedulers[0])
+
+    def test_underapprox_mc_cmaze_rmin(self):
+        program = stormpy.parse_prism_program(get_example_path("pomdp", "maze-concise.prism"))
+        formulas = stormpy.parse_properties_for_prism_program("R{\"steps\"}min=? [F ((x = 2) & (y = 0))]", program)
+        model = stormpy.build_model(program, formulas)
+        model = stormpy.pomdp.make_canonic(model)
+        options = stormpy.pomdp.BeliefExplorationModelCheckerOptionsDouble(False, True)
+        options.use_explicit_cutoff = True
+        options.size_threshold_init = 10
+        belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options);
+        result = belmc.check(formulas[0].raw_formula, [])
+        print(result.lower_bound)
+        print(result.upper_bound)
+        print(result.induced_mc_from_scheduler)
+        print(result.cutoff_schedulers[0])
