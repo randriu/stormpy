e6a44270373aa93b6599fbc91f1f60e871ffa3ca
Roman Andriushchenko <iandri@fit.vut.cz>
2022-09-16 20:11:18 +0200

    added dec-POMDP rewards

--- src/synthesis/decpomdp.cpp
+++ src/synthesis/decpomdp.cpp
@@ -11,6 +11,7 @@ void define_decpomdp(py::module& m) {
         // .def("construct_mdp", &storm::synthesis::PomdpManager<double>::constructMdp, "Unfold memory model (a priori memory update) into the POMDP.")
         .def_property_readonly("num_agents", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_agents;}, "number of agents")
         .def_property_readonly("num_states", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_states();}, "number of states")
+        .def_property_readonly("row_reward", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.row_reward;}, "for each row group, a list of row rewards")
         ;
 
     m.def("parse_decpomdp", &storm::synthesis::parseDecPomdp,  py::arg("filename"));
