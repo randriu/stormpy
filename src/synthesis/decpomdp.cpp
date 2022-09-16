3bfa315d34736c41d20162b4fc7442683ea72859
Roman Andriushchenko <iandri@fit.vut.cz>
2022-09-16 21:15:02 +0200

    discounted rewards

--- src/synthesis/decpomdp.cpp
+++ src/synthesis/decpomdp.cpp
@@ -8,9 +8,12 @@ void define_decpomdp(py::module& m) {
 
     py::class_<storm::synthesis::DecPomdp>(m, "DecPomdp", "dec-POMDP")
         // .def(py::init<std::string const&>(), "constructor.", py::arg("filename"));
-        // .def("construct_mdp", &storm::synthesis::PomdpManager<double>::constructMdp, "Unfold memory model (a priori memory update) into the POMDP.")
+        // .def("apply_discount_factor", &storm::synthesis::DecPomdp::applyDiscountFactor, "apply the discount factor", py::arg("discount"))
         .def_property_readonly("num_agents", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_agents;}, "number of agents")
         .def_property_readonly("num_states", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_states();}, "number of states")
+        .def_property_readonly("transition_matrix", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.transition_matrix;}, "transition matrix")
+        .def_property_readonly("discount", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.discount;}, "discount factor")
+        .def_property_readonly("reward_minimizing", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.reward_minimizing;}, "if True, the reward value is interpreted as a cost")
         .def_property_readonly("row_reward", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.row_reward;}, "for each row group, a list of row rewards")
         ;
 
