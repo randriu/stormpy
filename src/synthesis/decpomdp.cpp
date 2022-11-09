9cdeb8de016fe7d2767b8db77ced5eb2af793387
Roman Andriushchenko <iandri@fit.vut.cz>
2022-11-09 18:14:49 +0100

    optional discount factor transformation

--- src/synthesis/decpomdp.cpp
+++ src/synthesis/decpomdp.cpp
@@ -8,13 +8,18 @@ void define_decpomdp(py::module& m) {
 
     py::class_<storm::synthesis::DecPomdp>(m, "DecPomdp", "dec-POMDP")
         // .def(py::init<std::string const&>(), "constructor.", py::arg("filename"));
-        // .def("apply_discount_factor", &storm::synthesis::DecPomdp::applyDiscountFactor, "apply the discount factor", py::arg("discount"))
+        .def("construct_mdp", &storm::synthesis::DecPomdp::constructMdp, "construct the underlying MDP")
         .def_property_readonly("num_agents", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_agents;}, "number of agents")
-        .def_property_readonly("num_states", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_states();}, "number of states")
-        .def_property_readonly("transition_matrix", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.transition_matrix;}, "transition matrix")
-        .def_property_readonly("discount", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.discount;}, "discount factor")
+        // .def_property_readonly("num_states", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.num_states();}, "number of states")
+        // .def_property_readonly("transition_matrix", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.transition_matrix;}, "transition matrix")
+        // .def_property_readonly("row_reward", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.row_reward;}, "for each row group, a list of row rewards")
+
+        .def_property_readonly("reward_model_name", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.reward_model_name;}, "")
         .def_property_readonly("reward_minimizing", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.reward_minimizing;}, "if True, the reward value is interpreted as a cost")
-        .def_property_readonly("row_reward", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.row_reward;}, "for each row group, a list of row rewards")
+        .def_property_readonly("discount_factor", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.discount_factor;}, "")
+        
+        .def("apply_discount_factor_transformation", &storm::synthesis::DecPomdp::applyDiscountFactorTransformation, "")
+        .def_property_readonly("discount_sink_label", [](storm::synthesis::DecPomdp& decpomdp) {return decpomdp.discount_sink_label;}, "")
         ;
 
     m.def("parse_decpomdp", &storm::synthesis::parseDecPomdp,  py::arg("filename"));
