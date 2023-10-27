44a7e861ee4533c08a93fd2c7cbbdabb94fc9210
Roman Andriushchenko <iandri@fit.vut.cz>
2023-10-27 09:55:20 +0200

    move dec-POMDP parser to Stormpy

--- src/synthesis/synthesis.cpp
+++ src/synthesis/synthesis.cpp
@@ -2,11 +2,11 @@
 
 void define_synthesis(py::module& m) {
 
-    define_decpomdp(m);
     define_helpers(m);
     define_pomdp(m);
     define_pomdp_builder(m);
     
+    bindings_decpomdp(m);
     bindings_counterexamples(m);
     bindings_pomdp_family(m);
 }
--- src/synthesis/synthesis.h
+++ src/synthesis/synthesis.h
@@ -3,10 +3,10 @@
 #include "src/common.h"
 
 void define_synthesis(py::module& m);
-void define_decpomdp(py::module &m);
 void define_helpers(py::module &m);
 void define_pomdp(py::module &m);
 void define_pomdp_builder(py::module &m);
 
+void bindings_decpomdp(py::module &m);
 void bindings_counterexamples(py::module &m);
 void bindings_pomdp_family(py::module &m);
